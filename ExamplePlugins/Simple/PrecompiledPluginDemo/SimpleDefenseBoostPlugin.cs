d2e212a0d62f5c856ad31f85aeb630d7219bd3ed
TiberiumFusion <6332277+TiberiumFusion@users.noreply.github.com>
2020-07-12 02:27:38 -0400

    Formatting fix

--- ExamplePlugins/Simple/PrecompiledPluginDemo/SimpleDefenseBoostPlugin.cs
+++ ExamplePlugins/Simple/PrecompiledPluginDemo/SimpleDefenseBoostPlugin.cs
@@ -27,27 +27,27 @@ namespace TTPluginsExamples.Simple.PrecompiledPluginDemo
     public class SimpleDefenseBoostPlugin : HPlugin
     {
         public override void Initialize()
-		{
-			// Establish this plugin's internal Identity within the TTPlugins environment. Every plugin should have a unique internal Identity.
-			Identity.PluginName = "SimpleDefenseBoostPlugin";
-			Identity.PluginDescription = "Example plugin. Increases player defense by 200.";
-			Identity.PluginAuthor = "TiberiumFusion";
-			Identity.PluginVersion = new Version("1.0.0.0");
+        {
+            // Establish this plugin's internal Identity within the TTPlugins environment. Every plugin should have a unique internal Identity.
+            Identity.PluginName = "SimpleDefenseBoostPlugin";
+            Identity.PluginDescription = "Example plugin. Increases player defense by 200.";
+            Identity.PluginAuthor = "TiberiumFusion";
+            Identity.PluginVersion = new Version("1.0.0.0");
 
             HasPersistentSavedata = false; // This plugin doesn't use persistent savedata.
-		}
-        
+        }
+
         public override void ConfigurationLoaded(bool successfulConfigLoadFromDisk)
-		{
+        {
             // This plugin does not use persistent savedata, so we will ignore our Configuration property.
         }
-        
+
         public override void PrePatch()
-		{
+        {
             // Define our single patch operation.
             CreateHPatchOperation("Terraria.Player", "ResetEffects", "PostfixPatch", HPatchLocation.Postfix);
                 // We will patch Terraria.Player.ResetEffects into calling our custom PostfixPatch method after the original method finishes.
-		}
+        }
 
 
         // Our stub method to patch into Terraria
