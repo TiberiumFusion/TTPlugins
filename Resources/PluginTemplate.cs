785e73be1a903cae07bfc1c20073289d0338be3b
TiberiumFusion <6332277+TiberiumFusion@users.noreply.github.com>
2020-07-27 14:19:43 -0400

    Add Terraria and common XNA namespaces to plugin template

--- Resources/PluginTemplate.cs
+++ Resources/PluginTemplate.cs
@@ -9,6 +9,10 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using com.tiberiumfusion.ttplugins.HarmonyPlugins;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Terraria;
 
 namespace {{TEMPLATE_NAMESPACE}}
 {
