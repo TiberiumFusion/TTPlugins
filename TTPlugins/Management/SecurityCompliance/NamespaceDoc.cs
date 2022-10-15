1385c9680deba88bd431d78a20a0622a95390279
TiberiumFusion <6332277+TiberiumFusion@users.noreply.github.com>
2022-10-14 21:19:44 -0400

    Add namespace doc

---
+++ TTPlugins/Management/SecurityCompliance/NamespaceDoc.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace com.tiberiumfusion.ttplugins.Management.SecurityCompliance
+{
+    /// <summary>
+    /// Subset of the offline plugin management framework which provides security compliance testing of plugins.
+    /// </summary>
+
+    [System.Runtime.CompilerServices.CompilerGenerated]
+    class NamespaceDoc
+    {
+    }
+}
